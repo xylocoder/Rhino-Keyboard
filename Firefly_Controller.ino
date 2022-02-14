1180d629fb8837a4f01cbf2124d43f980b2a7863
Trevor McDonald <75909106+xylocoder@users.noreply.github.com>
2022-02-14 11:41:41 -0500

    Polyline Tool

--- Firefly_Controller.ino
+++ Firefly_Controller.ino
@@ -6,10 +6,14 @@ int force;
 const int sensor1 = A1;  // Analog input pin that the potentiometer is attached to
 const int sensor2 = A2;  // Analog input pin that the potentiometer is attached to
 const int sensor3 = A3;  // Analog input pin that the potentiometer is attached to
+const int sensor4 = A1;  // Analog input pin that the potentiometer is attached to
+const int sensor5 = A2;  // Analog input pin that the potentiometer is attached to
 
 int sensorValue1 = 0;        // value read from the pot
 int sensorValue2 = 0;        // value read from the pot
 int sensorValue3 = 0;        // value read from the pot
+int sensorRotationValue4 = 0;        // value read from the pot
+int sensorDistanceValue5 = 0;        // value read from the pot
 
 
 void setup() {
@@ -22,6 +26,8 @@ void loop() {
   sensorValue1 = analogRead(sensor1);
   sensorValue2 = analogRead(sensor2);
   sensorValue3 = analogRead(sensor3);
+  sensorRotationValue4 = analogRead(sensor4);
+  sensorDistanceValue5 = analogRead(sensor5);
   force = analogRead(pressurePin);
 
   Serial.print(sonar.ping_cm());
@@ -32,6 +38,12 @@ void loop() {
   Serial.print(",");
   Serial.print(sensorValue3);
   Serial.print(",");
-  Serial.println(force);
+  Serial.print(force);
+  Serial.print(sensorRotationValue4);
+  Serial.print(",");
+  Serial.print(sensorDistanceValue5);
+  Serial.println(",");
   delay(100);
 }
+
+
